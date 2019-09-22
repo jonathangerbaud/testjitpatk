1cc690a46db0ce0179c60ac25cd5b3bae7693c99
Jonathan Gerbaud <jonathan.gerbaud@gmail.com>
2019-09-22 13:53:06 +0200

    Added some class because sublib is not available

---
+++ plugins/sublib2/src/main/java/com/toolsfordevs/sublib2/Sublib2.kt
@@ -0,0 +1,14 @@
+package com.toolsfordevs.sublib2
+
+import com.toolsfordevs.sublib3.Sublib3
+
+
+class Sublib2
+{
+    val sublib = Sublib3()
+
+    init
+    {
+        sublib.doSomeStuff()
+    }
+}
---
+++ plugins/sublib3/src/main/java/com/toolsfordevs/sublib3/Sublib3.kt
@@ -0,0 +1,17 @@
+package com.toolsfordevs.sublib3
+
+
+class Sublib3
+{
+    val desc = "say hello"
+
+    init
+    {
+
+    }
+
+    fun doSomeStuff()
+    {
+        val a = 45
+    }
+}
